042e07e4e4375408452d0e91f174f587cd07af05
ISSMOULI <ssmouliindani@gmail.com>
2025-09-06 22:45:24 +0530

    just commit

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+//using System.Data;
+//using System.IO.Pipelines;
 namespace HelloWOrld
 {
     class Program
@@ -6,6 +8,16 @@ namespace HelloWOrld
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine(5 + 5);
+            Console.WriteLine(10 - 5);
+            Console.WriteLine(5 * 5);
+            Console.WriteLine(10 / 2);
+            Console.WriteLine(10 % 3);
+            Console.WriteLine(10 / 3);
+            int a = 11;
+            int b = 2;
+            int result = a / b;
+            Console.WriteLine(result);
         }
     }
 }
