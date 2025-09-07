59e6e79b45e73c592fcba81d4b1957893b721967
ISSMOULI <ssmouliindani@gmail.com>
2025-09-07 19:57:40 +0530

    commit

---
+++ print-statements.cs
@@ -0,0 +1,13 @@
+Console.WriteLine("Hello World"); // prints hello world
+Console.Write("this line will be printed in new line."); // prints in the above line
+Console.WriteLine(" this line will be printed in the same line"); // prints in the new line
+long mynum = 15000000000; // data type long
+Console.WriteLine(mynum);
+bool isCSharpfun = true; //data type boolean
+bool isfishtasty = false;
+Console.WriteLine(isCSharpfun); // prints true
+Console.WriteLine(isfishtasty); // prints false
+string username = Console.ReadLine(); // takes user input
+int userage = Console.ReadLine(); // takes user input
+Console.WriteLine(username); // prints user input
+Console.WriteLine(userage); // prints user input
