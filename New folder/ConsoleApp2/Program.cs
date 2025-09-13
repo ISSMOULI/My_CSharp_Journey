0c1a64a2bfa00a444db066d9632b5257bdb75321
ISSMOULI <ssmouliindani@gmail.com>
2025-09-13 17:09:42 +0530

    type casting

--- New folder/ConsoleApp2/Program.cs
+++ New folder/ConsoleApp2/Program.cs
@@ -1,2 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+// type casting 
+int a = 10;
+double b = a; // implicit casting
+Console.WriteLine(b.GetType());
+Console.WriteLine(b);
