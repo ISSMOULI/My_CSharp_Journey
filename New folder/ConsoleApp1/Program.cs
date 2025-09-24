9ceb6dcf2dab93ef8af310e89d695bdf6b79ac2f
ISSMOULI <ssmouliindani@gmail.com>
2025-09-24 19:46:58 +0530

    commit

--- New folder/ConsoleApp1/Program.cs
+++ New folder/ConsoleApp1/Program.cs
@@ -19,18 +19,24 @@ namespace HelloWOrld
             int result = a / b;
             Console.WriteLine(result);
             long mynum = 15000000000; // data type long
-Console.WriteLine(mynum);
-bool isCSharpfun = true; //data type boolean
-bool isfishtasty = false;
-Console.WriteLine(isCSharpfun); // prints true
+            Console.WriteLine(mynum);
+            bool isCSharpfun = true; //data type boolean
+            bool isfishtasty = false;
+            Console.WriteLine(isCSharpfun); // prints true
             Console.WriteLine(isfishtasty); // prints false
-Console.WriteLine(Math.Max(5, 10));
-Console.WriteLine(Math.Min(5, 10));
-Console.WriteLine(Math.Sqrt(64));      
-Console.WriteLine(Math.Abs(-4.7));
-Console.WriteLine(Math.Round(4.6));
-Console.WriteLine(Math.Ceiling(4.2));
-Console.WriteLine(Math.Floor(4.7));
+            Console.WriteLine(Math.Max(5, 10));
+            Console.WriteLine(Math.Min(5, 10));
+            Console.WriteLine(Math.Sqrt(64));      
+            Console.WriteLine(Math.Abs(-4.7));
+            Console.WriteLine(Math.Round(4.6));
+            Console.WriteLine(Math.Ceiling(4.2));
+            Console.WriteLine(Math.Floor(4.7));
+
+
+            bool sCSharpfun = true; //data type boolean
+            bool sfishtasty = false;
+            Console.WriteLine(sCSharpfun); // prints true
+            Console.WriteLine(sfishtasty); // prints false
         }
     }
 }
